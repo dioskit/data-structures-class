e73dc952d952e2b49b128f4dc917a5d0fd250112
dioskit <154680296+dioskit@users.noreply.github.com>
2024-03-05 15:11:00 +0530

    update

--- stack-questions/divyanshbwing101.c
+++ stack-questions/divyanshbwing101.c
@@ -1,3 +1,22 @@
+/********************************
+ * write a function hanoi(n, start, end) 
+ * n is number of disk
+ * start is start rod
+ * end is end rod
+ * assumptions
+ * 1 <= start <= 3
+ * 1 <= end <= 3
+ * start != end
+ * n >= 1
+ * 
+ * 
+ * pm(start, end) := print(start,->,end)
+***************************************/
+
+
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -5,6 +24,27 @@ struct node {
     int data;
     struct node* next;
 };
+typedef struct node Node;
+Node * rod_one = NULL; 
+Node * rod_two = NULL; 
+Node * rod_three = NULL; 
+
+void pm(Node* start, Node* end){
+    while(start != NULL || end != NULL){
+        printf("%d to %d",start -> data, end -> data);
+        start = start -> next;
+        end  = end -> next;
+    }
+    printf("\n");
+}
+
+void hanoi(int n, Node *start, Node*end){
+    if(n == 1){
+        pm(start,end)
+    }
+}
+
+
 int main(){
     
 }
