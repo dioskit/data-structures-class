44230cca15d215f2d8a6aa0a6c649c44177c9699
dioskit <154680296+dioskit@users.noreply.github.com>
2024-03-30 17:44:52 +0530

    code fixed and running well

--- names_of_god.c
+++ names_of_god.c
@@ -1,24 +1,52 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
+#include <stdlib.h>
 
+#define MAX_SIZE 100
 
-void generate_sums(int target, int current_sum, int current_combo[],int start){ //start is initially 1.
-    if(current_sum == target){
-        printcombo(current_combo); // function to print the combinations;
+int top = -1;
+
+void generate_sums(int target, int current_sum, int current_combo[], int start);
+void printcombo(int current_combo[], int length);
+void push(int current_combo[], int i);
+void pop();
+
+int main() {
+    int num = 10 ;
+    int current_combo[MAX_SIZE];
+    generate_sums(num, 0, current_combo, 1);
+    return 0;
+}
+
+void generate_sums(int target, int current_sum, int current_combo[], int start) {
+    if (current_sum == target) {
+        printcombo(current_combo, top + 1);
         return;
     }
-    if(current_sum > target){
+    if (current_sum > target) {
         return;
     }
-    for(int i = start, i < target + 1, i++){
-        push(current_combo,i);
-        generate_sums(target,current_sum + 1, current_combo[], i);
-        pop(current_combo);
+    for (int i = start; i <= target + 1; i++) {
+        push(current_combo, i);
+        generate_sums(target, current_sum + i, current_combo, i);
+        pop();
     }
 }
-int main(){
-    int num = 3;
-int current_combo[MAX_SIZE];
+
+void printcombo(int current_combo[], int length) {
+    for (int i = 0; i < length; i++) {
+        if (i + 1 == length) {
+            printf("%d \n", current_combo[i]);
+        } else {
+            printf("%d + ", current_combo[i]);
+        }
+    }
+}
+
+void push(int current_combo[], int i) {
+    top++;
+    current_combo[top] = i; 
+}
+
+void pop() {
+    top--;
 }
