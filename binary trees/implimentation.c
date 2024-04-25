61c132500c5022ae91f54a5c8934135b23909a2f
dioskit <154680296+dioskit@users.noreply.github.com>
2024-04-25 22:57:50 +0530

    Update implimentation.c

--- binary trees/implimentation.c
+++ binary trees/implimentation.c
@@ -7,8 +7,10 @@ struct Node{
     struct Node* left;
     struct Node* right;
 };
+
 typedef struct Node Node;
 Node* tree;
+
 void create_tree(Node*);
 Node* insertElement(Node*, int);
 void preorderTraversal(Node*);
@@ -91,9 +93,11 @@ int main(){
     getch();
     return 0;
 }
+
 void create_tree(Node* tree){
     tree = NULL;
 }
+
 Node* insertElement(Node* tree, int val){
     Node *ptr, *nodeptr, *parentptr;
     ptr = (Node*)malloc(sizeof(Node));
@@ -118,6 +122,7 @@ Node* insertElement(Node* tree, int val){
     }
     return tree;
 }
+
 void preorderTraversal(Node* tree){
     if(tree!=NULL){
         preorderTraversal(tree->left);
@@ -125,6 +130,7 @@ void preorderTraversal(Node* tree){
         printf("%d\t",tree->data);
     }
 }
+
 void inorderTraversal(Node* tree){
     if(tree!-NULL){
         inorderTraversal(tree->left);
@@ -132,19 +138,122 @@ void inorderTraversal(Node* tree){
         inorderTraversal(tree->right);
     }
 }
+
 void postorderTraversal(Node*tree){
     if(tree!= NULL){
         postorderTraversal(tree->left);
-        
+        postorderTraversal(tree->right);
+        printf("%d\t",tree->data);
     }
 }
-Node* findSmallestElement(Node*);
-Node* findLargestElement(Node*);
-Node* deleteElement(Node*,int);
-Node* mirrorImage(Node*);
-int totalNodes(Node*);
-int totalExternal(Node*);
-int totalInternal(Node*);
-int height(Node*);
-Node* deleteTree(Node);
+
+Node* findSmallestElement(Node* tree){
+    if(tree == NULL || (tree->left == NULL)) return tree;
+    else return findSmallestElement(tree->left);
+}
+
+Node* findLargestElement(Node* tree){
+    if(tree == NULL || (tree->right == NULL)) return tree;
+    else findLargestElement(tree->right); 
+}
+
+Node* deleteElement(Node* tree,int val){
+    Node *cur, *parent, *suc, *psuc, *ptr;
+    if(tree->left == NULL){
+        printf("\n The tree is empty");
+        return tree;
+    }
+    parent = tree;
+    cur = tree->left;
+    while(cur!=NULL && val!=cur->data){
+        parent = cur;
+        cur = (val < cur->data)? cur->right;
+    }
+    if(cur == NULL){
+        printf("\n the value to be deleted is not present in the tree");
+        return(tree);
+    }
+    if(cur->left == NULL) ptr = cur->right;
+    else if(cur->right == NULL) ptr = cur -> left;
+    else{
+        //find the in-order sucessor and its parent
+        psuc = cur;
+        cur = cur->left;
+        while(suc->left!=NULL){
+            psuc = suc;
+            suc = suc->left;
+        }
+        if(cur == psuc){
+            //situation 1
+            suc -> left = cur->right;
+        }
+        else{
+            //situation 2
+            suc->left = cur->left;
+            psuc->left = suc->right;
+            suc->right = cur->right;
+        }
+        ptr = suc;
+    }
+    // attact ptr to the parent node
+    if(parent->left == cur)
+        parent->left = ptr;
+    else
+        parent->left = ptr;
+    free(cur);
+    return tree;
+}
+
+int totalNodes(Node* tree){
+    if(tree == NULL)
+        return 0;
+    else
+        return(totalNodes(tree->left) + totalNodes(tree->right) + 1);
+}
+
+int totalExternal(Node* tree){
+    if((tree==NULL)||((tree->left==NULL) && (tree->right==NULL)))
+        return 0;
+    else
+        return(totalInternal(tree->left)+totalInternal(tree->right)+1);
+}
+
+int totalInternal(Node* tree){
+    if((tree==NULL)||((tree->left==NULL)&&(tree->right==NULL)))
+        return 0;
+    else
+        return (totalInternal(tree->left)+totalInternal(tree->right)+1);
+}
+
+int height(Node* tree){
+    int leftheight, rightheight;
+    if(tree==NULL) return 0;
+    else{
+        leftheight = Height(tree->left);
+        rightheight = height(tree->right);
+        if(leftheight > rightheight)
+            return (leftheight + 1);
+        else
+            return (rightheight + 1);
+    }
+}
+
+Node* mirrorImage(Node* tree){
+    struct Node* ptr;
+    if(tree!=NULL){
+        mirrorImage(tree->left);
+        mirrorImage(tree->right);
+        ptr = tree->left;
+        ptr->left = ptr->right;
+        tree->right = ptr;
+    }
+}
+
+Node* deleteTree(Node* tree){
+    if(tree!=NULL){
+        deleteTree(tree->left);
+        deleteTree(tree->right);
+        free(tree);
+    }
+}
 
